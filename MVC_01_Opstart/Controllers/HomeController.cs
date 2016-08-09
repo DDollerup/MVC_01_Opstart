4a5024ee2dd02e3f5923318031e2c90a29239d85
PDDaniel <daniel@proper-dog.com>
2016-08-09 09:14:50 +0200

    Save

--- MVC_01_Opstart/Controllers/HomeController.cs
+++ MVC_01_Opstart/Controllers/HomeController.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC_01_Opstart.Models;
+using MVC_01_Opstart.Factories;
 
 namespace MVC_01_Opstart.Controllers
 {
     public class HomeController : Controller
     {
+        ProductFactory productFac;
+        CategoryFactory categoryFac;
+
         // GET: Home
         public ActionResult Index()
         {
@@ -22,51 +26,15 @@ namespace MVC_01_Opstart.Controllers
 
         public ActionResult Products()
         {
-            Product p1 = new Product();
-            p1.ID = 1;
-            p1.Name = "Nike Sko";
-            p1.Description = "This is a nice shoe, if you like that kind of shoe.";
-            p1.Price = 39.99;
-            p1.CategoryID = 1;
-
-            Product p2 = new Product();
-            p2.ID = 2;
-            p2.Name = "Scholl Bicycle";
-            p2.Description = "This is a nice Bicycle, if you like that kind of Bicycle.";
-            p2.Price = 1999.99;
-            p2.CategoryID = 2;
-
-            List<Product> allProducts = new List<Product>();
-            allProducts.Add(p1);
-            allProducts.Add(p2);
-
-
-
+            productFac = new ProductFactory();
+            List<Product> allProducts = productFac.GetAll();
             return View(allProducts);
         }
 
         public ActionResult ShowProduct(int id = 0)
         {
-            Product p1 = new Product();
-            p1.ID = 1;
-            p1.Name = "Nike Sko";
-            p1.Description = "This is a nice shoe, if you like that kind of shoe.";
-            p1.Price = 39.99;
-            p1.CategoryID = 1;
-
-            Product p2 = new Product();
-            p2.ID = 2;
-            p2.Name = "Scholl Bicycle";
-            p2.Description = "This is a nice Bicycle, if you like that kind of Bicycle.";
-            p2.Price = 1999.99;
-            p2.CategoryID = 2;
-
-            List<Product> allProducts = new List<Product>();
-            allProducts.Add(p1);
-            allProducts.Add(p2);
-
-            Product productToFind = allProducts.Find(product => product.ID == id);
-
+            productFac = new ProductFactory();
+            Product productToFind = productFac.Get(id);
             return View(productToFind);
         }
     }
