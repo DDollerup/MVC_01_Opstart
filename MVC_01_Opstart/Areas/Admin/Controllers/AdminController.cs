32482b5203db2d5a34a472cd066dd6f2436d3cbc
PDDaniel <daniel@proper-dog.com>
2016-08-09 13:25:46 +0200

    Add Product

---
+++ MVC_01_Opstart/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_01_Opstart.Factories;
+using MVC_01_Opstart.Models;
+
+namespace MVC_01_Opstart.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        ProductFactory productFac;
+        CategoryFactory categoryFac;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddProduct()
+        {
+            categoryFac = new CategoryFactory();
+            ViewBag.AllCategories = categoryFac.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProductSubmit(Product p)
+        {
+            productFac = new ProductFactory();
+            productFac.Add(p);
+
+            TempData["MSG"] = "A new product, " + p.Name + ", has been added.";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
